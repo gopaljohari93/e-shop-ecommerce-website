7c424319a47ef34392eac85c6bc64b9a2e9e74b4
Gopal Johari <gopaljohari007@gmail.com>
2026-01-22 11:33:02 +0530

    first commit

---
+++ src/assets/mockData.jsx
@@ -0,0 +1,82 @@
+import Image1 from "../assets/images/bag1.webp";
+import Image2 from "../assets/images/sandle.webp";
+import Image3 from "../assets/images/watch.webp";
+import Image4 from "../assets/images/cap.webp";
+import Image5 from "../assets/images/boot.webp";
+import Image6 from "../assets/images/dovecare.webp";
+import Image7 from "../assets/images/headphone.webp";
+import Image8 from "../assets/images/robot.webp";
+import Image9 from "../assets/images/camera3.webp";
+import Image10 from "../assets/images/tshirt.webp";
+
+export const Categories = [
+  "Electronics",
+  "Fashion",
+  "Home & Kitchen",
+  "Beauty",
+  "Sports",
+  "Automotive",
+];
+
+export const mockData = [
+  {
+    id: 1,
+    image: Image1,
+    name: "Medium 30 L Laptop Bag",
+    price: 29.99,
+  },
+  {
+    id: 2,
+    image: Image9,
+    name: "SUTY 4K Sports Camera",
+    price: 35.99,
+  },
+  {
+    id: 3,
+    image: Image3,
+    name: "Hampton Analog Watch",
+    price: 32.99,
+  },
+  {
+    id: 4,
+    image: Image4,
+    name: "Self Design Woven Beanie",
+    price: 49.99,
+  },
+  {
+    id: 5,
+    image: Image8,
+    name: "Remote Control Robot",
+    price: 69.99,
+  },
+  {
+    id: 6,
+    image: Image6,
+    name: "Product 1",
+    price: 29.99,
+  },
+  {
+    id: 7,
+    image: Image7,
+    name: "Product 1",
+    price: 29.99,
+  },
+  {
+    id: 8,
+    image: Image8,
+    name: "Product 1",
+    price: 29.99,
+  },
+  {
+    id: 9,
+    image: Image9,
+    name: "Product 1",
+    price: 29.99,
+  },
+  {
+    id: 10,
+    image: Image10,
+    name: "Product 1",
+    price: 29.99,
+  },
+];
